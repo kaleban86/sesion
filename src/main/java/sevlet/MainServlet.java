f6b1c9efbd63680f7d8bf32000f4eac035a2173e
Nikolai <nikolaignap@gmail.com>
2020-12-24 22:53:25 +0300

    Test

--- src/main/java/sevlet/MainServlet.java
+++ src/main/java/sevlet/MainServlet.java
@@ -1,10 +1,7 @@
 package sevlet;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.ServletSecurity;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.net.URLEncoder;
 
 @ServletSecurity
 public class MainServlet extends javax.servlet.http.HttpServlet {
@@ -13,12 +10,25 @@ public class MainServlet extends javax.servlet.http.HttpServlet {
     protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse response)
             throws javax.servlet.ServletException, IOException {
 
-        HttpSession session = req.getSession();
-        session.setAttribute("name", "<script>\n" +
-                "    alert(\"У вас открыта вкладка \")\n" +
-                "</script>");
-        getServletContext().getRequestDispatcher("/test.jsp").forward(req, response);
+
+        int s = 1;
+        int d = 0;
+        if (s > d) {
+
+
+
+
+        }
     }
+
+
+    public void test(){
+
+        System.out.println("rrr");
+    }
+
+
+
 }
 
 
